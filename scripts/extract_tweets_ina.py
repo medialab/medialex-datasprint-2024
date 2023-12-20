7e8da93ff651c830a2b78a1b3a3480868a09d315
bmaz <beatrice.mazoyer@gmail.com>
2023-12-20 14:17:01 +0100

    Work only at day-file level

--- scripts/extract_tweets_ina.py
+++ scripts/extract_tweets_ina.py
@@ -8,13 +8,13 @@ import glob
 import json
 
 from datetime import datetime
-from twitwi import normalize_tweet
-from multiprocessing import Manager, Pool
+from multiprocessing import Pool
 
 
-def read_file(args):
-    in_file, tweet_dict = args
+def read_file(in_file):
     print(datetime.now(), in_file)
+
+    tweet_dict = {}
     with gzip.open(in_file, 'rb') as f:
         for i, line in enumerate(f):
 
@@ -22,12 +22,16 @@ def read_file(args):
 
             if "id" in tweet:
 
-                normalized = normalize_tweet(tweet, extract_referenced_tweets=True)
+                sub_ids = [int(tweet["id"])]
                 ts = int(ts)
 
-                for subtweet in normalized:
-                    sub_id = int(subtweet["id"])
+                if 'retweeted_status' in tweet and tweet['retweeted_status']['id_str'] != sub_ids[0]:
+                    sub_ids.append(int(tweet['retweeted_status']['id_str']))
 
+                elif 'quoted_status' in tweet and tweet['quoted_status']['id_str'] != sub_ids[0]:
+                    sub_ids.append(int(tweet['quoted_status']['id_str']))
+
+                for sub_id in sub_ids:
                     if sub_id in tweet_dict:
 
                         previous_ts = tweet_dict[sub_id]
@@ -36,26 +40,21 @@ def read_file(args):
                     else:
                         tweet_dict[sub_id] = ts
 
-            if i % 1000000 == 0:
+            if i % 100000 == 0:
                 print(datetime.now(), in_file, round(100*i/8472140, 2), len(tweet_dict))
+
+    with open(in_file.replace(".gz", "_unique_ids.csv"), "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "last_timestamp"])
+        for key, value in tweet_dict.items():
+            writer.writerow([key, value])
 
 if __name__ == '__main__':
     files_path = sys.argv[1]
     nb_processes = int(sys.argv[2])
 
-    with Manager() as manager:
 
-        tweet_dict = manager.dict()
-
-        with Pool(processes=nb_processes) as pool:
-            pool.imap_unordered(read_file, ((file, tweet_dict) for file in sorted(glob.glob(files_path))))
-            pool.close()
-            pool.join()
-
-            print(len(tweet_dict))
-            with open("tweet_dict.csv", "w") as f:
-                writer = csv.writer(f)
-                writer.writerow(["id", "last_timestamp"])
-                for key, value in tweet_dict.items():
-                    writer.writerow([key, value])
-
+    with Pool(processes=nb_processes) as pool:
+        pool.imap_unordered(read_file, sorted(glob.glob(files_path)))
+        pool.close()
+        pool.join()
